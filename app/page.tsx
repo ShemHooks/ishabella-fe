6641820fa3c6cf277b8dbc3a4b93b5db4a17cc1b
unknown <shemregidor07@gmail.com>
2026-03-02 23:38:22 +0800

    modified component in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/LandingPage/Hero";
 import Advert from "@/components/LandingPage/Advert";
+import ProductCategory from "@/components/LandingPage/ProductCategory";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Advert />
+      <ProductCategory />
     </div>
   );
 }
--- components/LandingPage/ProductCategory.tsx
+++ components/LandingPage/ProductCategory.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const ProductCategory = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text-center mb-4">
+        <h1>Product Categories</h1>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default ProductCategory;
